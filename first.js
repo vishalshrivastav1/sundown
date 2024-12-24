571e7176cb22c346ae29664fb46ff024426a93ca
Vishal <shrivastavvishal854@gmail.com>
2024-12-23 20:24:42 -0800

    first

--- first.js
+++ first.js
@@ -2,3 +2,15 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+
+var elemc = document.querySelector(".elem-container")
+var fixed = document.querySelector(".fixed-image")
+elemc.addEventListener("mouseenter",function(){
+    fixed.style.display = "block"
+})
+
+elemc.addEventListener("mouseenter",function(){
+    fixed.style.display = "none"
+})
+
